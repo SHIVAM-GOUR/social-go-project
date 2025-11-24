b45985da328eb97ae825eecca228eef19fac7cfc
Riya Garg <riya.garg@data-route.com>
2025-11-24 16:31:23 +0530

    filtering, sorting and pagination done

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,21 +1,42 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"social/internal/store"
+)
 
-func (app  *application) getUserFeedHandler (w http.ResponseWriter, r *http.Request){
+func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
+
+	// pagination, filtering and sorting
+
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+
+	}
 
 	ctx := r.Context()
 
-	
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(5))
-	if err != nil{
-		app.internalServerError(w,r,err)
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(5), fq)
+	if err != nil {
+		app.internalServerError(w, r, err)
 		return
 	}
 
-	if err := app.jsonResponse(w, http.StatusOK, feed); err != nil{
-		app.internalServerError(w,r,err)
-		
+	if err := app.jsonResponse(w, http.StatusOK, feed); err != nil {
+		app.internalServerError(w, r, err)
+
 	}
 
 }
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -30,7 +30,7 @@ type PostStore struct {
 	db *sql.DB
 }
 
-func (s *PostStore) GetUserFeed(ctx context.Context, userID int64) ([]PostWithMetaData, error) {
+func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedFeedQuery) ([]PostWithMetaData, error) {
 
 	query := `
         SELECT 
@@ -45,17 +45,22 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64) ([]PostWithMe
             COUNT(c.id) AS comments_count
         FROM posts p
         LEFT JOIN comments c ON c.post_id = p.id
-        JOIN followers f ON f.follower_id = p.user_id
-        JOIN users u ON u.id = p.user_id
-        WHERE f.follower_id = $1 OR p.user_id = $1
+		LEFT JOIN users u ON p.user_id = u.id
+        JOIN followers f ON f.follower_id = p.user_id OR p.user_id = $1
+        
+        WHERE 
+		f.user_id = $1 AND 
+		(p.title ILIKE '%' || $4 || '%' OR p.content ILIKE '%' || $4 || '%') AND
+		(p.tags @> $5 OR $5 = '{}')
         GROUP BY p.id, u.username
-        ORDER BY p.created_at DESC
+        ORDER BY p.created_at ` + fq.Sort + `
+		LIMIT $2 OFFSET $3
     `
 
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeOutDuration)
 	defer cancel()
 
-	rows, err := s.db.QueryContext(ctx, query, userID)
+	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset, fq.Search, pq.Array(fq.Tags))
 	if err != nil {
 		return nil, err
 	}
